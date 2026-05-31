2f1598d2771a6f1e2164dd3d008ddba55f80932c
miacro <fqguozhou@gmail.com>
2026-05-31 14:56:41 +0800

    update v2raya log

--- containers/net-proxy/v2raya/run-rootless.sh
+++ containers/net-proxy/v2raya/run-rootless.sh
@@ -2,10 +2,11 @@
 podman run -d \
   --name net-proxy-v2raya \
   --restart always \
-  -p 2018:2017 \
+  -p 2017:2017 \
   -p 20170:20170 \
   -p 20171:20171 \
   -p 20172:20172 \
+  --tmpfs /var/log/v2raya:rw,size=100m \
   -v /etc/localtime:/etc/localtime:ro \
   -v ~/.config/v2raya:/etc/v2raya:Z \
   net-proxy/v2raya
